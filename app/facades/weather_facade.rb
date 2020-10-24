829b3d08f0440890672c0fc2c815e3047d14c99a
Nico Rithner <nicorithner@gmail.com>
2020-10-23 19:47:04 -0600

    Remove pry left from previous testing

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,7 +1,6 @@
 class WeatherFacade
   def self.get_forecast(zip)
     results = WeatherService.get_forecast_data(zip)
-    # binding.pry
     destination_data =  {temp: results[:main][:temp], temp_min: results[:main][:temp_min], temp_max: results[:main][:temp_max], description: results[:weather][0][:description] }
 
     Weather.new(destination_data)
