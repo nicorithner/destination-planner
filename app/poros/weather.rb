160cd74e32ecda29f5b7e51babf4cd6d6e11d7c9
Nico Rithner <nicorithner@gmail.com>
2020-10-28 18:45:02 -0600

    Create weather poro

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -1,9 +1,9 @@
 class Weather
   attr_reader :temp, :temp_max, :temp_min, :description
   def initialize(data)
-    @temp = data[:temp].round
-    @temp_max = data[:temp_max]
-    @temp_min = data[:temp_min]
-    @description = data[:description]
+    @temp = data[:main][:temp].round
+    @temp_max = data[:main][:temp_max]
+    @temp_min = data[:main][:temp_min]
+    @description = data[:weather][0][:description]
   end
 end
