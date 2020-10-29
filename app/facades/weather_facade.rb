a44b103229b58e117110838e54724787e173ef27
Nico Rithner <nicorithner@gmail.com>
2020-10-28 18:46:59 -0600

    Update weather facade

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -1,8 +1,9 @@
 class WeatherFacade
   def self.get_forecast(zip)
     results = WeatherService.get_forecast_data(zip)
-    destination_data =  {temp: results[:main][:temp], temp_min: results[:main][:temp_min], temp_max: results[:main][:temp_max], description: results[:weather][0][:description] }
+    # destination_data =  {temp: results[:main][:temp], temp_min: results[:main][:temp_min], temp_max: results[:main][:temp_max], description: results[:weather][0][:description] }
 
-    Weather.new(destination_data)
+    Weather.new(results)
+    # Weather.new(destination_data)
   end
 end
