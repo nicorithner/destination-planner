33570d7d59401e1f66eea840aff7ca6a8927c0aa
Nico Rithner <nicorithner@gmail.com>
2020-10-28 18:41:47 -0600

    Create api/v1/destination_controller

---
+++ app/controllers/api/v1/destinations_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::DestinationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render json: DestinationSerializer.new(Destination.all)
+  end
+
+  def show
+    render json: DestinationSerializer.new(Destination.find(params[:id]))
+  end
+
+  def create
+    render json: DestinationSerializer.new(Destination.create(destination_params)), status: :created
+
+  end
+
+  def update
+    render json: DestinationSerializer.new(Destination.update(params[:id], destination_params))
+  end
+
+  def destroy
+    destination = Destination.find(params[:id])
+    render json: DestinationSerializer.new(destination.delete)
+    render json: {}, status: :no_content
+  end
+
+  private
+    def destination_params
+      params.require(:destination).permit(
+                                    :id,
+                                    :name,
+                                    :zip,
+                                    :description,
+                                    :image_url
+                                  )
+    end
+end
