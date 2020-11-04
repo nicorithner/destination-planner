0e1ab78a69e91bfe3995f80eb697dc3ab846fbaf
Nico Rithner <nicorithner@gmail.com>
2020-11-04 08:51:38 -0700

    Add api/v1/destinations routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root 'destinations#index'
+  namespace :api do
+    namespace :v1 do
+      resources :destinations
+    end
+  end
   resources :destinations
 end
