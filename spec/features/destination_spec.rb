7e8d4ee4567c807c577ad635810b1148f40d9535
Nico Rithner <nicorithner@gmail.com>
2020-11-03 19:27:38 -0700

    Update test to fit use of DestinationData poro

--- spec/features/destination_spec.rb
+++ spec/features/destination_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe "Destination Show Page" do
   describe "As a Visitor" do
     before :each do
-      @dest_1 = Destination.create!( name: 'Denver', zip: '80202', description: 'Some place', image_url: 'https://place-puppy.com/300x300' )
-      response = WeatherService.get_weather(@dest_1)
-      @weather = Weather.new(response)
-
+      @dest_1 = Destination.create!( id: 1, name: 'Denver', zip: '80202', description: 'Some place', image_url: 'https://place-puppy.com/300x300' )
+      @destination = DestinationFacade.get_destination_data(@dest_1.id)
     end
 
     it "Can see destination's info" do
@@ -16,15 +14,15 @@ RSpec.describe "Destination Show Page" do
         expect(current_path).to eq("/destinations/#{@dest_1.id}")
       end
 
-      expect(page).to have_content(@dest_1.name)
-      expect(page).to have_content(@dest_1.zip)
-      expect(page).to have_content(@dest_1.description)
+      expect(page).to have_content(@destination.name)
+      expect(page).to have_content(@destination.zip)
+      expect(page).to have_content(@destination.description)
 
       within(".forecast") do
-        expect(page).to have_content(@weather.temperature)
-        expect(page).to have_content(@weather.high)
-        expect(page).to have_content(@weather.low)
-        expect(page).to have_content(@weather.description)
+        expect(page).to have_content(@destination.temperature)
+        expect(page).to have_content(@destination.high)
+        expect(page).to have_content(@destination.low)
+        expect(page).to have_content(@destination.summary)
       end
     end
   end
