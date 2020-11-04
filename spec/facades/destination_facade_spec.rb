5f03e3128c8b749dee0771338b2fb8b67903bcdd
Nico Rithner <nicorithner@gmail.com>
2020-11-03 19:34:39 -0700

    Fix tests typo

--- spec/facades/destination_facade_spec.rb
+++ spec/facades/destination_facade_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "DestinationFacade" do
     data = Destination.create!(id: "1", name: 'Denver', zip: '80202', description: 'Some place')
     @destination_data = DestinationFacade.get_destination_data(data[:id])
 
-    expect(@destination_data).to be_a(Destination)
+    expect(@destination_data).to be_a(DestinationData)
     expect(@destination_data.name).to be_a(String)
     expect(@destination_data.zip).to be_a(String)
     expect(@destination_data.description).to be_a(String)
