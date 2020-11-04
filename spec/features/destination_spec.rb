3f8a3d77311ccf82bf1115112289bceec8463cb7
Nico Rithner <nicorithner@gmail.com>
2020-11-03 17:46:22 -0700

    Change weather.weather to weather.temperature

--- spec/features/destination_spec.rb
+++ spec/features/destination_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe "Destination Show Page" do
   describe "As a Visitor" do
     before :each do
       @dest_1 = Destination.create!( name: 'Denver', zip: '80202', description: 'Some place', image_url: 'https://place-puppy.com/300x300' )
-      @weather = WeatherService.get_weather(@dest_1)
+      response = WeatherService.get_weather(@dest_1)
+      @weather = Weather.new(response)
+
     end
 
     it "Can see destination's info" do
@@ -17,7 +19,7 @@ RSpec.describe "Destination Show Page" do
       expect(page).to have_content(@dest_1.description)
 
       within(".forecast") do
-        expect(page).to have_content(@weather.weather)
+        expect(page).to have_content(@weather.temperature)
         expect(page).to have_content(@weather.high)
         expect(page).to have_content(@weather.low)
         expect(page).to have_content(@weather.description)
