4df312f0e4f3bd2298b47d8c23c64ee303af166c
Nico Rithner <nicorithner@gmail.com>
2020-11-04 08:52:16 -0700

    Add index, show and create actions

---
+++ app/controllers/api/v1/destinations_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::DestinationsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  def index
+    render json: DestinationSerializer.new(Destination.all)
+  end
+
+  def show
+    render json: DestinationSerializer.new(Destination.find(params[:id]))
+  end
+
+  def create
+    render json: DestinationSerializer.new(Destination.create(destination_params)), status: :created
+  end
+
+  private
+    def destination_params
+      params.permit( :id, :name, :zip, :description, :image_url )
+    end
+end
