14bf7485f03a2d7af270772709eb185a32a7bbe1
Nico Rithner <nicorithner@gmail.com>
2020-11-04 09:13:56 -0700

    Add update action

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -12,6 +12,10 @@ class Api::V1::DestinationsController < ApplicationController
     render json: DestinationSerializer.new(Destination.create(destination_params)), status: :created
   end
 
+  def update
+    render json: DestinationSerializer.new(Destination.update(params[:id], destination_params))
+  end
+
   private
     def destination_params
       params.permit( :id, :name, :zip, :description, :image_url )
