eee9410aca2a81cfea2fca4003a57a6c83561462
Nico Rithner <nicorithner@gmail.com>
2020-11-03 19:36:32 -0700

    Upgrade destination_facade
    Upgraded to pass on all destination data

--- app/facades/destination_facade.rb
+++ app/facades/destination_facade.rb
@@ -2,11 +2,11 @@ class DestinationFacade
   def self.get_destination_data(id)
     destination = Destination.find(id)
     weather = WeatherFacade.get_weather(destination)
-    binding.pry
-    image = ImageService.new(weather.summary)
+    image_json = ImageService.get_image(weather.summary)
+    image = image_json[:data][0][:images][:original][:url]
     
-    all_data = {name: destination.name, zip: destination.zip, description: destination.description, temperature: weather.temperature, high: weather.high, low: weather.low, summary: weather.summary image_url: image}
-    
+    all_data = {name: destination.name, zip: destination.zip, description: destination.description, temperature: weather.temperature, high: weather.high, low: weather.low, summary: weather.summary, image_url: image}
+
     DestinationData.new(all_data)
   end
 end
